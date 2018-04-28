e0ed915c1f16e455404215b58e80f9e489599d09
joaonsantos <joaopns05@gmail.com>
2018-04-28 16:57:47 +0100

    Added file to install vim comp deps.

---
+++ install-vim-deps.sh
@@ -0,0 +1,21 @@
+# File to install vim deps
+
+sudo apt-get install build-essential \
+    libncurses5-dev libncursesw5-dev \
+    libatk1.0-dev libx11-dev \
+    libxpm-dev libxt-dev libxtst-dev
+
+
+# Vim configuration command
+# ./configure --with-features=huge \
+#             --enable-multibyte \
+#             --enable-rubyinterp=yes \
+#             --enable-python3interp=yes \
+#             --with-python3-config-dir=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu \
+#             --enable-perlinterp=yes \
+#             --enable-luainterp=yes \
+#             --enable-gui=gtk2 \
+#             --enable-cscope \
+#             --prefix=/usr/local \
+#             --with-x=yes \
+#             --with-compiledby="João Santos"
